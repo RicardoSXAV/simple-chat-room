4d0895bf27bd428b823baa22f504f0b88c40ec75
Ricardo dos Santos Xavier <ricardosantosxav@gmail.com>
2022-10-07 15:17:40 -0300

    refactor: change formatMessageTime util

--- src/utils/messages.ts
+++ src/utils/messages.ts
@@ -3,5 +3,5 @@ import { formatDistanceToNowStrict } from "date-fns";
 export const formatMessageTime = (createdAt: string) => {
   const messageTime = new Date(createdAt);
 
-  return formatDistanceToNowStrict(new Date(createdAt), { addSuffix: true });
+  return formatDistanceToNowStrict(messageTime, { addSuffix: true });
 };
